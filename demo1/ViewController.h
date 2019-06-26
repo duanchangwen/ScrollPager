46c375fa0aea66671e08c4a537b14720b29a075b
duanchangwen <changwen.duan@ambow.com>
2019-06-26 17:29:13 +0800

    Initial Commit

---
+++ demo1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo1
+//
+//  Created by dcw on 2019/6/26.
+//  Copyright © 2019 ambow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ demo1/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  demo1
+//
+//  Created by dcw on 2019/6/26.
+//  Copyright © 2019 ambow. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
